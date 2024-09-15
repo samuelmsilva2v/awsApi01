bc0f6ec57393e969ab1953a55221daf978ba1737
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-09-15 15:35:59 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/CorsConfig.java
@@ -0,0 +1,16 @@
+package br.com.cotiinformatica;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("http://aws-site01.s3-website-us-east-1.amazonaws.com/")
+				.allowedMethods("POST", "PUT", "DELETE", "GET").allowedHeaders("*");
+	}
+}
